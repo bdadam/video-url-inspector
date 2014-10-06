c0cf068f55be44f818d1f7e6e13f21e955f29d4a
bdadam <bdadam@gmail.com>
2014-10-07 00:40:22 +0200

    added browserify build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,24 @@
 var gulp = require('gulp');
-var mocha = require('gulp-mocha');
 
 gulp.task('test', function () {
+	var mocha = require('gulp-mocha');
     return gulp.src('tests/services.js', { read: false }).pipe(mocha({reporter: 'list'}));
 });
+
+gulp.task('browserify', function() {
+	var browserify = require('gulp-browserify');
+	var rename = require('gulp-rename');
+
+    return gulp.src('./index.js')
+		        .pipe(browserify({
+		          debug : false,
+		          standalone: 'videoUrlInspector',
+		          transform: [
+		          	[{ global: true }, 'uglifyify']
+		          ]
+		        }))
+		        .pipe(rename('videoUrlInspector.js'))
+		        .pipe(gulp.dest('build'))
+});
+
+gulp.task('build', ['test', 'browserify']);
